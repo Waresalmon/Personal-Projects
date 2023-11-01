3de463008d230ecc47dc1f81b26d87ad2b24533b
Ben <benjaminsalmon2003@gmail.com>
2023-11-01 16:32:06 -0600

    Commented alarm.py and updated README

--- alarm-clock/README.txt
+++ alarm-clock/README.txt
@@ -10,4 +10,7 @@ You will be asked if you would like to set an alarm or timer. When the desired t
 Assumptions: User understands 24H format. A sound output is set up to allow 
 program to play '.mp3' file and for it to be heard.
 
+Note: 'maze.mp3' is not owned by me. Music is owned by 'Alisha_Sutudo'.
+    Music can be found at 'https://pixabay.com/music/beats-maze-173204/'
+
 Created by: Benjamin Salmon
--- alarm-clock/alarm.py
+++ alarm-clock/alarm.py
@@ -1,12 +1,21 @@
 
 import datetime as d
 import os
+#Removes welcome message from pygame
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 from pygame import mixer
 
 
 
 def alarm_or_timer():
+    """Asks User if they would like to set an alarm or a timer
+    
+    Arguments:
+        none
+        
+    Returns: 
+        'Alarm' or 'Timer'. Depends on users input
+    """
     dummy = True
     while(dummy):
         answer = input('Set "Alarm" or Set "Timer": ').strip()
@@ -17,29 +26,50 @@ def alarm_or_timer():
             dummy = False
             return answer
         else:
-            print("Not a given input. Try again. Press 'Q' to quit")
+            print("Not a given input. Try again. Press 'Q' to quit program")
             assert (answer != 'Q'), 'Quitting program'
 
 def set_alarm():
+    """Grabs time from user and sets off alarm when time is reached
+    
+    Arguments:
+        none
+        
+    Returns: 
+        none
+    """
+    
     print('What time would you like the alarm to be set for?')
     alarm_time = input("Please use 24H format of 'HH:MM:SS': ")
-    #need to vefify that alarm_time is inputted correctly
+
+    #Validate if time exists
     while (validate(alarm_time) == 'FORMAT_ERROR'):
         print('What time would you like the alarm to be set for?')
         alarm_time = input("Please use 24H format of 'HH:MM:SS': ")
-    #grab time from computer
-    print('Alarm set for: ', alarm_time)
+        
     current_time = str(d.datetime.now().strftime("%H:%M:%S"))
+    
+    print('Alarm set for: ', alarm_time)
     while(alarm_time != current_time):
         current_time = str(d.datetime.now().strftime("%H:%M:%S"))
     play_sound()
     
+    return
+    
 def validate(a):
-    #Checks inputted format is obtainable in 24H time.
+    """Veifies that the time the user inputted is obtainable.
+    
+    Arguments:
+        a: the time the user inputted in a 24H format
+        
+    Returns: 
+        'FORMAT_ERROR' if time the user inputted is unobtainable
+    """
     split = a.split(':')
     if(len(a) > 8):
         print("Too many inputs. Please reinput.",'\n')
         return 'FORMAT_ERROR'
+    
     for i in range(len(split)):
         if(i == 0):
             if(int(split[i]) >= 24):
@@ -73,9 +103,16 @@ def validate(a):
             return
 
 def play_sound():
+    """Seraches current directory for a '.mp3' file. If file is
+    found it will play that file. Otherwise it will play
+    the default '.mp3' file. Uses 'pygame' module to play '.mp3' file
     
-    #need to look for mp3 file in directoary
-    #then play mp3
+    Arguments:
+        none
+        
+    Returns: 
+        none
+    """
     mp3count = 0
     mixer.init()
     for file in os.listdir(os.getcwd()):
@@ -112,13 +149,23 @@ def play_sound():
     return
 
 def timer():
-
+    """Grabs time from user and sets off timer.
+    
+    Arguments:
+        none
+        
+    Returns: 
+        none
+    """
     print('How long would you like to set a timer for?')
     timer_time = input("Please use 24H format of 'HH:MM:SS': ")
+    
     while (validate(timer_time) == 'FORMAT_ERROR'):
         print('What time would you like the alarm to be set for?')
-        timer_time = input("Please use 24H format of 'HH:MM:SS': ")    
+        timer_time = input("Please use 24H format of 'HH:MM:SS': ")  
+          
     current_time = str(d.datetime.now().strftime("%H:%M:%S"))
+    #Add user inputted time to current time
     timer_time = timer_addition(timer_time, current_time)
     print('Timer set for: ', timer_time[0],':',timer_time[1],':',timer_time[2])
     while(timer_time != current_time):
@@ -128,36 +175,54 @@ def timer():
     return
 
 def timer_addition(t,c):
+    """Adds user inputted time to the current time. This new time will
+    be used to determine when '.mp3' will go off
+    
+    Arguments:
+        t: user inputted time
+        c: current time
+        
+    Returns: 
+        timer_split: the sum of the current time and the user inputted time
+    """
     timer_split = t.split(':')
     current_split = c.split(':')
+    
+    #add time for seconds and minutes
     for i in range(len(timer_split),0,-1):
         timer_split [i-1] = str(int(timer_split[i-1]) + int(current_split[i-1]))
         if(int(timer_split[i-1]) > 60 ):
             timer_split[i-1] = str(int(timer_split[i-1]) - 60 )
             timer_split[i-2] = str(int(timer_split[i-2]) + 1)
+            
             if(int(timer_split[i-1]) < 10):
                 timer_split[i-1] = str(0) + str(timer_split[i-1])
+                
+    #Time is in 24 hour. turn to 00 instead of 24            
     if(int(timer_split[0]) > 23):
         timer_split[0] = str(int(timer_split[0]) - 24)
+        
         if(int(timer_split[0]) < 10):
-            timer_split[0] = str(0 + timer_split[0])
+            timer_split[0] = str(0) + timer_split[0]
         
     return timer_split
 
 def main():
+    """Main function. Program can run from this function
+    
+    Arguments:
+        none
+        
+    Returns: 
+        none
+    """
     choice = alarm_or_timer()
     if(choice == 'Alarm'):
         set_alarm()
+        
     elif(choice == 'Timer'):
         timer()
+    return
     
 if __name__ == '__main__':
-    #making an alarm clock that plays a sound at a set time or in x minutes
-    #can pass through function. But more readable this way
-    #choice = alarm_or_timer()
-    #can call in alarm_or_time function
-    #if(choice == 'Alarm'):
-    #    set_alarm()
-    #elif(choice == 'Timer'):
-    #timer()
     main()
